f81ee154ca3f500a4219b12ce23bc0063c72a6bd
Isaac The Brave <53839630+Isaac-The-Brave@users.noreply.github.com>
2022-09-06 23:48:18 +0200

    Add files via upload

---
+++ animalLogic/AttackLogic.java
@@ -0,0 +1,34 @@
+package animalLogic;
+
+import java.util.ArrayList;
+import worldLogic.EvolveCell;
+
+public class AttackLogic {
+
+	public AttackLogic(ArrayList<Cell> cellList){
+		ArrayList<Cell> deadCells = new ArrayList<>();
+		EvolveCell evolutionLogic = new EvolveCell();
+		
+		for (Cell cell2 : cellList)
+			for (Cell cell: cellList){
+				// if the cell is on the same tile as a nute, collect it
+				if (cell.getPos().equals(cell2.getPos()) && cell.getNutrientsInSystem() > cell2.getNutrientsInSystem()) {  				 
+					cell.setKillCount(cell.getKillCount() + 4);
+					evolutionLogic.EvolveGenetic(cell.getLastFiredSequence(), cell.getBaseCellGenetics(), cell);
+					deadCells.add(cell2);
+					
+				}
+				else if(cell.getPos().equals(cell2.getPos()) && cell.getNutrientsInSystem() < cell2.getNutrientsInSystem()){
+					cell2.setKillCount(cell.getKillCount() + 4);
+					evolutionLogic.EvolveGenetic(cell2.getLastFiredSequence(), cell2.getBaseCellGenetics(), cell2);
+					deadCells.add(cell);
+				}
+
+			}cellList.removeAll(deadCells);
+
+
+	}
+
+}
+
+
