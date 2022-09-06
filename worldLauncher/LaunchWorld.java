fee226be213c26aa9c313d23a4e4ed458dc5c67b
Isaac The Brave <53839630+Isaac-The-Brave@users.noreply.github.com>
2022-09-06 20:53:14 +0200

    Update LaunchWorld.java

--- worldLauncher/LaunchWorld.java
+++ worldLauncher/LaunchWorld.java
@@ -45,9 +45,7 @@ public class LaunchWorld {
 
 public static void main(String[] args) {
 	// invokeLater() is used here to prevent our graphics processing from
-	// blocking the GUI. https://stackoverflow.com/a/22534931/4655368
-	// this is a lot of boilerplate code that you shouldn't be too concerned about.
-	// just know that when main runs it will call initWindow() once.
+	// blocking the GUI.
 		SwingUtilities.invokeLater(new Runnable() {
 			public void run() {
 				initWindow();
